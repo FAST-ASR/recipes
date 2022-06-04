a44804204b584cefb48dc33db4c6c712899f7242
Kocour Martin <ikocour@fit.vutbr.cz>
2022-06-04 17:40:23 +0200

    Run script

---
+++ datasets/wsj/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run WSJ experiment with LF-MMI implemented in MarkovModels.jl
+# Author: Martin Kocour (ikocour@fit.vut.cz)
+
+# Extract features
+julia --project="./" script/feats_preparation.jl
+
+# Prepare graphs
+julia --project="./" script/graph_preparation.jl
+
+# Train model
+julia --project="./" script/model_training.jl
